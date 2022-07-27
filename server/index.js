c0e01913274728af83cfd84ac04b57addef32e2f
Freya Caudwell <freyacaudwell@gail.com>
2022-07-27 14:32:07 +0100

    router initial set-up

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = new express();
 require('dotenv').config();
+const cors = require('cors');
+const router = require('./router');
 
 const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Express is running on Port ${port}`);
