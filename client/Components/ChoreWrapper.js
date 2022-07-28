23d29bd6b0b923eba513b3b49fe649876cfe1819
Freya Caudwell <freyacaudwell@gail.com>
2022-07-28 18:05:45 +0100

    schedule view rendering chores

--- client/Components/ChoreWrapper.js
+++ client/Components/ChoreWrapper.js
@@ -3,13 +3,14 @@ import { styles } from './Styles/ChoreWrapperStyles';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import ChoreDetails from './ChoreDetails';
 
-export default function ChoreWrapper() {
+export default function ChoreWrapper({ chore }) {
+  console.log('ChoreWrapper chore', chore);
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <View>
-          <Text style={styles.text}>Clean oven</Text>
-          <Text style={[styles.text, styles.subText]}>Kitchen</Text>
+          <Text style={styles.text}>{chore.taskName}</Text>
+          <Text style={[styles.text, styles.subText]}>{chore.room}</Text>
         </View>
         <TouchableOpacity style={styles.button}>
           <FontAwesome name="info" style={styles.icon} />
@@ -18,7 +19,7 @@ export default function ChoreWrapper() {
           <FontAwesome name="check" style={styles.icon} />
         </TouchableOpacity>
       </View>
-      <ChoreDetails />
+      <ChoreDetails chore={chore} />
     </View>
   );
 }
--- client/Pages/AllChoresView.js
+++ client/Pages/AllChoresView.js
@@ -1,11 +1,27 @@
 import { NavBar, ChoreWrapper } from '../Components/index';
 import { Text, View } from 'react-native';
 import { styles } from './Styles/AllChoresViewStyles';
+import { useState, useEffect } from 'react';
+import ApiClientService from '../Services/ApiClientService';
 
 export default function AllChoresView({ navigation }) {
+  const [choreData, setChoreData] = useState([]);
+
+  async function getChoreData() {
+    const data = await ApiClientService.getAllChores();
+    setChoreData(data);
+    console.log('All chores', data);
+  }
+
+  useEffect(() => {
+    getChoreData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <ChoreWrapper />
+      {choreData.map((chore) => (
+        <ChoreWrapper chore={chore} key={chore._id} />
+      ))}
       <NavBar navigation={navigation} />
     </View>
   );
