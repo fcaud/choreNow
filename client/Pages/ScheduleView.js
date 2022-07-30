971b6a636b9a15b27f65b46fea484b0939f6257a
Freya Caudwell <freyacaudwell@gail.com>
2022-07-30 14:24:45 +0100

    schedule view rendering tasks across days

--- client/Pages/ScheduleView.js
+++ client/Pages/ScheduleView.js
@@ -1,5 +1,5 @@
 import NavBar from '../Components/NavBar';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import { styles } from './Styles/ScheduleViewStyles';
 import moment from 'moment';
 import { useState, useEffect } from 'react';
@@ -39,38 +39,63 @@ export default function ScheduleView({ navigation }) {
   useEffect(() => {
     const fetchData = async () => {
       const settings = await getSettings();
-      await getRankedChores();
-      addSettingsToWeekAhead(settings);
+      const chores = await getRankedChores();
+      const weekData = await addDataToWeekAhead(settings, chores);
+      // addChoresToWeekAhead(chores);
     };
     fetchData();
   }, []);
-  //map through days and chores and if time space in a day render chores
-  function addSettingsToWeekAhead(settings) {
-    //add time settings to weekAhead
+
+  function addDataToWeekAhead(settings, chores) {
     setWeekAhead((oldVal) => {
       let days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
       let res = { ...oldVal };
-      Object.values(weekAhead).map((data, i) => {
+      //add time settings to weekAhead
+      Object.values(oldVal).map((data, i) => {
         let lookUpDayIndex = new Date(data.date).getDay();
         data = { ...data, time: settings[days[lookUpDayIndex]] };
         res = { ...res, [i + 1]: data };
       });
+      //map through days and chores and if time space in a day add to chore array in weekAhead
+      let allocatedChores = [];
+      Object.values(res).map((data, i) => {
+        chores.map((chore) => {
+          //pick out chore duration < time available
+          if (
+            chore.timeToComplete <= data.time - data.timeUsed &&
+            !allocatedChores.includes(chore._id) &&
+            chore.status !== 'Not due'
+          ) {
+            //add chore to chore array
+            data.chores.push(chore);
+            //Prevent chore from being reselected
+            allocatedChores.push(chore._id);
+            //update time used
+            data.timeUsed = data.timeUsed + chore.timeToComplete;
+          }
+        });
+      });
       return res;
     });
   }
+
   return (
     <View style={styles.container}>
       <Text>ScheduleView</Text>
-      {Object.values(weekAhead).map((day) => {
-        return (
-          <View key={day.date}>
-            <Text key={day.date}>{moment(day.date).format('ddd Do MMM')}</Text>
-            {day.chores.map((chore) => (
-              <ChoreWrapper chore={chore} key={chore._id} />
-            ))}
-          </View>
-        );
-      })}
+      <ScrollView>
+        {Object.values(weekAhead).map((day) => {
+          return (
+            <View key={day.date}>
+              <Text key={day.date}>
+                {moment(day.date).format('ddd Do MMM')}
+              </Text>
+              {day.chores.map((chore) => (
+                <ChoreWrapper chore={chore} key={chore._id} />
+              ))}
+            </View>
+          );
+        })}
+      </ScrollView>
       <NavBar navigation={navigation} />
     </View>
   );
