d8abd7cf51c066ab52795fb75476690d6d530b1d
Freya Caudwell <freyacaudwell@gail.com>
2022-08-26 19:26:57 +0100

    clean up

--- client/Components/Styles/TimeInputStyles.js
+++
@@ -1,5 +0,0 @@
-import { StyleSheet } from 'react-native';
-import globalStyles from '../../Utils/GlobalStylingVariables';
-
-const styles = StyleSheet.create({});
-export { styles };
--- client/Features/Styles/UserTimePrefFormStyles.js
+++ client/Features/Styles/UserTimePrefFormStyles.js
@@ -1,8 +1,6 @@
 import { StyleSheet } from 'react-native';
-import globalStyles from '../../Utils/GlobalStylingVariables';
 
 const styles = StyleSheet.create({
-  container: {},
   p: { marginRight: 15, width: 100 },
   row: { alignItems: 'space-between' },
 });
